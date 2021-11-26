729279cc2766cee0a01f46ea165ef0980043e7cb
GUO CHIH-LING <3a832098@gm.student.ncut.edu.tw>
2021-11-27 00:06:04 +0800

    DB migrate，練習3-1 使用model查詢資料

--- app/Http/Controllers/AdminPostsController.php
+++ app/Http/Controllers/AdminPostsController.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\Post;
 
 class AdminPostsController extends Controller
 {
     public function index()
     {
+        $posts = Post::orderBy('created_at','DESC')->get();
+        $data = ['posts'=>$posts];
+
         return view('admin.posts.index');
     }
 
